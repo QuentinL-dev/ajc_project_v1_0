a9d21f1d554f87e5e22d2ec2a7488bfff30f43cc
QuentinL-dev <qlaverton@gmail.com>
2021-05-17 11:24:13 +0200

    Commit 2

--- install-pyhttp/build-image/pyapp/http_server.py
+++ install-pyhttp/build-image/pyapp/http_server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# openssl req -nodes -new -x509 -keyout key.pem -out cert.pem
+
 import os
 import ssl
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -21,20 +23,18 @@ class PythonHTTPServer:
         self.server = HTTPServer(("", self.port), self.handler)
 
     def check_SSL_certificate(self):
-        if os.path.isfile('./key.pem') and os.path.isfile('./cert.pem'):
+        if os.path.isfile('key.pem') and os.path.isfile('cert.pem'):
             return True
         return False
 
-    def generate_SSL_certificate(self):
-        os.system('openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365')
-
     def run(self):
-        if not self.check_SSL_certificate():
-            self.generate_SSL_certificate()
-        self.server.socket = ssl.wrap_socket(self.server.socket, keyfile="key.pem", certfile="cert.pem", server_side=True)
-        self.server.serve_forever()
+        if self.check_SSL_certificate():
+            self.server.socket = ssl.wrap_socket(self.server.socket, keyfile="key.pem", certfile="cert.pem", server_side=True)
+            self.server.serve_forever()
+        else:
+            print("Python HTTP Server Error - Cannot read SSL certificates")
 
 
 if __name__ == '__main__':
-    httpd = PythonHTTPServer
+    httpd = PythonHTTPServer()
     httpd.run()
